572f84a64317afdc5f66fa6352f61c4f1c57957b
dsutton47 <dominic.sutton47@gmail.com>
2021-04-01 21:49:39 -0700

    Changes to PowerShellService

--- DockerGUI/Service/PowerShellService.cs
+++ DockerGUI/Service/PowerShellService.cs
@@ -12,46 +12,43 @@ namespace DockerGUI.Service
     {
         public async System.Threading.Tasks.Task<Collection<PSObject>> addImageAsync()
         {
-            // Start an instance of Power Shell
-            using (PowerShell ps = PowerShell.Create())
-            {
-                // Set Commands and Parameters for Power Shell
-                ps.AddCommand("Set-ExecutionPolicy")
-                  .AddParameter("ExecutionPolicy", "Bypass")
-                  .AddParameter("Scope", "Process")
-                  .AddParameter("Force");
+            // Create an empty process 
+            Process process;
+            // Set the process to be a single instance of Powershell with a docker remove command for an image
+            ProcessStartInfo processInfo = new ProcessStartInfo($@"C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe", $@"docker image ls");
 
-                // Add script to retrive a list of Images from Power Shell
-                ps.AddCommand(@"docker image ls");
+            // Parameters for the process instance, CreateNoWindow is commented out for testing purposes
+            //processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            process = Process.Start(processInfo);
 
-                Collection<PSObject> result = ps.Invoke();
+              Collection<PSObject> result = process;
                 // outputObject contains the result of the powershell script
                 foreach (var outputObject in result)
                 {
-                    // Testing for a successful retrival of the image list
+                    // Testing for successful retrival of the container list
                     using StreamWriter file = new StreamWriter("C:\\Test\\ImageListTest.txt");
                     string container = outputObject.ToString();
                     await file.WriteLineAsync(container);
                 }
-                return result;
-            }
+            //the line below locks the original form window until the powershell window is closed
+            process.Close();
+            return result;
         }
 
         public async System.Threading.Tasks.Task<Collection<PSObject>> addContainerAsync()
         {
-            // Start an instance of Power Shell
-            using (PowerShell ps = PowerShell.Create())
-            {
-                // Set Commands and Parameters for Power Shell
-                ps.AddCommand("Set-ExecutionPolicy")
-                  .AddParameter("ExecutionPolicy", "Bypass")
-                  .AddParameter("Scope", "Process")
-                  .AddParameter("Force");
+             // Create an empty process 
+            Process process;
+            // Set the process to be a single instance of Powershell with a docker remove command for an image
+            ProcessStartInfo processInfo = new ProcessStartInfo($@"C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe", $@"docker container ls");
 
-                // Add script to retrive a list of Containers from Power Shell
-                ps.AddCommand(@"docker container ls");
+            // Parameters for the process instance, CreateNoWindow is commented out for testing purposes
+            //processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            process = Process.Start(processInfo);
 
-                Collection<PSObject> result = ps.Invoke();
+              Collection<PSObject> result = process;
                 // outputObject contains the result of the powershell script
                 foreach (var outputObject in result)
                 {
@@ -60,8 +57,10 @@ namespace DockerGUI.Service
                     string container = outputObject.ToString();
                     await file.WriteLineAsync(container);
                 }
-                return result;
-            }
+            //the line below locks the original form window until the powershell window is closed
+            process.Close();
+            return result;
+            
         }
 
         public void removeImage(string imgName)
